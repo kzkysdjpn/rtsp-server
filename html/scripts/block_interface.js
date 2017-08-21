8f7ca0e8ccc35febef89d2df554a07b73298c4c8
Kazuki Yoshida <kzkysdjpn@gmail.com>
2017-08-22 00:04:18 +0900

    Check reboot process. The reboot process issue not found at 64bit Win7 OS envronment.....

--- html/scripts/block_interface.js
+++ html/scripts/block_interface.js
@@ -8,7 +8,7 @@ function open_block_ui(msg)
 			backgroundColor: '#000', 
 			'-webkit-border-radius': '10px', 
 			'-moz-border-radius': '10px', 
-			opacity: .5, 
+			opacity: .9, 
 			color: '#fff' 
 		}
 	}); 
